d0f9138bbf4e8f3138af16b993564da6d5b3ab6c
henisusilowati23 <henisusilowati2301@gmail.com>
2023-05-25 19:34:37 +0700

    first commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Management</title>
+
+    <style>
+        table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Aksi</th>
+                <th>Avatar</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Role</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>1</td>
+                <td>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </td>
+                <td>
+                    <img src="https://placehold.co/200x200" alt="Avatar">
+                </td>
+                <td>Admin</td>
+                <td>admin@gmail.com</td>
+                <td>0857306444</td>
+                <td>Administrator</td>
+        </tbody>
+    </table>
+</body>
+</html>
